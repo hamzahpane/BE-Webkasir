3ad35d54a5ff81e213325f8cbfc304f408a23456
hamzahpane <m.hamzahpane27@gmail.com>
2024-10-21 18:18:22 +0700

    first commit

--- database/index.js
+++ database/index.js
@@ -1,22 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { dbHost, dbPort, dbName } = require("../app/config");
 
-const {  dbHost ,dbPort , dbUser  ,dbName ,dbPass} = require('../app/config');
-
-console.log(dbUser , dbPass , dbHost , dbPort , dbName );
-
-
-const mongodbURI = `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`;
-console.log(mongodbURI);
-
-mongoose.connect(mongodbURI, {
-
-});
-
+mongoose
+  .connect(`mongodb://${dbHost}:${dbPort}/${dbName}`)
+  .then(() => console.log("Connected!"))
+  .catch((error) => console.error("MongoDB connection error:", error));
 
 const db = mongoose.connection;
 
-db.on('error', (error) => {
-    console.error('MongoDB connection error:', error);
+db.on("error", (error) => {
+  console.error("MongoDB connection error:", error);
 });
 
 module.exports = db;
